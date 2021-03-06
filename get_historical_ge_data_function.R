6c9d0bffb1b3a4e716d88e719a60071a5ec5354b
Brenden-Mucklow <39507517+Brenden-Mucklow@users.noreply.github.com>
2021-03-06 12:15:57 -0800

    Add files via upload

---
+++ get_historical_ge_data_function.R
@@ -0,0 +1,24 @@
+library(httr)
+library(anytime)
+library(tidyr)
+
+get_historical_ge_data = function(endpoint)
+{
+  endpoint0 = paste("exchange/history/rs/all?name=", endpoint, sep = "")
+  
+  url = modify_url("https://api.weirdgloop.org/", path = endpoint0)
+  
+  response = GET(url)
+  
+  if ( http_error(response) ){
+    print(status_code(response))
+    stop("Something went wrong.", call. = FALSE)
+  }
+  
+  json_text = content(response,"text")
+  item_data_from_json = jsonlite::fromJSON(json_text)
+  item_data = item_data_from_json[[1]]
+  item_data = item_data[,!(names(item_data) == "volume")]
+  item_data$timestamp = as.Date(anytime(item_data$timestamp/1000))
+  item_data
+}
